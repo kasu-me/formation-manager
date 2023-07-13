c2bbd01b88930fcfb18626d06ccbec30bdf82f6c
unknown <morooka@kasu.me>
2023-07-13 10:53:03 +0900

    各所にアイコンを追加

--- js/defineDialog.js
+++ js/defineDialog.js
@@ -22,9 +22,9 @@ window.addEventListener("load", function () {
 				table.addRow();
 				table.addCell(`${seriesList[seriesId].name}`, { "class": "formation-name" });
 				table.addCell(`${seriesList[seriesId].description}`, { "class": "formation-template-name" });
-				table.addCell(`<button onclick="">編集</button>`);
-				table.addCell(`<button onclick="">編成一覧</button>`);
-				table.addCell(`<button onclick="">編成テンプレート一覧</button>`);
+				table.addCell(`<button class="lsf-icon" icon="pen" onclick="">編集</button>`);
+				table.addCell(`<button class="lsf-icon" icon="list" onclick="">編成一覧</button>`);
+				table.addCell(`<button class="lsf-icon" icon="list" onclick="">編成テンプレート一覧</button>`);
 			}
 			document.querySelector("#seriesDispDialog div.table-container").innerHTML = table.generateTable();
 			Dialog.list.seriesDispDialog.on();
