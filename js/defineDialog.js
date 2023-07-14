360505822697dc998d283b8b0d908c8722122e85
unknown <morooka@kasu.me>
2023-07-14 16:31:25 +0900

    車両詳細ダイアログから所属編成へジャンプできるリンクを追加

--- js/defineDialog.js
+++ js/defineDialog.js
@@ -331,7 +331,7 @@ window.addEventListener("load", function () {
 			}
 			table.addRow();
 			table.addCell(`所属編成`);
-			table.addCell(`${formation != -1 ? AllFormations.formationsList[formation].name : "編成に所属していません"}<small> (${now.toStringWithLink()}時点)</small>`);
+			table.addCell(`${formation != -1 ? `<a href="javascript:Dialog.list.formationDetealDialog.functions.display(${formation})">${AllFormations.formationsList[formation].name}</a>` : "編成に所属していません"}<small> (${now.toStringWithLink()}時点)</small>`);
 			table.addRow();
 			table.addCell("車歴");
 			table.addCell(oldNumbersText);
