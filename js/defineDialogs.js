9f63b39de33dbb6c65ed519bcdbb5308ac8e00c9
unknown <morooka@kasu.me>
2023-08-23 11:43:54 +0900

    マスタ一覧画面でシフトキーによる選択を実装

--- js/defineDialogs.js
+++ js/defineDialogs.js
@@ -911,6 +911,8 @@ window.addEventListener("load", function () {
 		let selectAllCheckBox = tableContainer.querySelector("tr td input[type='checkbox']");
 		//各行のチェックボックス
 		let checkboxes = tableContainer.querySelectorAll("tr:not(:nth-child(1)) td input[type='checkbox']");
+		//最後にチェックした行
+		let lastCheckedRow = -1;
 		//各行のステータスに応じてボタンの活性･非活性を切り替え
 		let switchElementsByCheckedStatus = () => {
 			let checkedAtLeastOnce = false;
@@ -941,16 +943,36 @@ window.addEventListener("load", function () {
 				if (!checkBox.checked) {
 					selectAllCheckBox.checked = false;
 				}
+				lastCheckedRow = findElementIndex(checkboxes, checkBox);
 				switchElementsByCheckedStatus();
 			});
 		});
 
 		//各行をクリックでチェックボックスをチェック
 		tableContainer.querySelectorAll("tr:not(:nth-child(1)) td:not(:first-child):not(:last-child)").forEach((td) => {
-			td.addEventListener("click", () => {
-				td.parentNode.querySelector("input[type='checkbox']").click();
+			td.addEventListener("click", (e) => {
+				if (e.shiftKey) {
+					window.getSelection().removeAllRanges();
+					let thisCheckBoxIndex = findElementIndex(checkboxes, td.parentNode.querySelector("input[type='checkbox']"));
+					let start = Math.min(lastCheckedRow, thisCheckBoxIndex);
+					let end = Math.max(lastCheckedRow, thisCheckBoxIndex);
+					let direction = checkboxes[lastCheckedRow].checked;
+					checkboxes[lastCheckedRow].click();
+					for (let i = start; i <= end; i++) {
+						if (checkboxes[i].checked != direction) {
+							checkboxes[i].click();
+						}
+					}
+				} else {
+					td.parentNode.querySelector("input[type='checkbox']").click();
+				}
+				window.getSelection().removeAllRanges();
 			});
 		});
+
+		let findElementIndex = (from, target) => {
+			return [].slice.call(from).indexOf(target);
+		};
 	}
 
 	//車両マスタデータ編集:edmsc
