b0a38d5df0280f7e5dea8a22a375c55f1b69b952
unknown <morooka@kasu.me>
2023-09-03 13:23:11 +0900

    メイン画面で備考が存在しない場合ハイフンを表示するよう変更
    備考欄が空欄だと見栄えが悪いので

--- js/ui.js
+++ js/ui.js
@@ -69,7 +69,7 @@ function list() {
 			//組成年月セルを追加
 			tables.at(-1).addCell(formationList[formationId].formatedOn.toStringWithLink());
 			//備考セルを追加
-			tables.at(-1).addCell(formationList[formationId].remark);
+			tables.at(-1).addCell(formationList[formationId].remark == "" ? "-" : formationList[formationId].remark);
 		}
 		tables.at(-1).addBlankCellToRowIn(1);
 		html += tables.at(-1).generateTable();
