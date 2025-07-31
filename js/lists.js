d3f4b145b6fcd3920fc0fa7f0aab44f5c491d988
ahozura <66002768+kasu-me@users.noreply.github.com>
2025-07-31 18:40:31 +0900

    正規表現のカッコがない場合を考慮

--- js/lists.js
+++ js/lists.js
@@ -269,8 +269,12 @@ class AllFormationTemplates {
 
 //(UIからの利用を想定)次の番号を取得
 function nextNumber(pattern) {
-	const regExp = new RegExp(`^${pattern}$`);
-	return pattern.split("(")[0] + (Number(AllCars.getNewestNumberByRegExp(regExp)) + 1);
+	if (!pattern.includes("(") || !pattern.includes(")")) {
+		return "";
+	} else {
+		const regExp = new RegExp(`^${pattern}$`);
+		return pattern.split("(")[0] + (Number(AllCars.getNewestNumberByRegExp(regExp)) + 1);
+	}
 }
 
 //全クラスリセット
